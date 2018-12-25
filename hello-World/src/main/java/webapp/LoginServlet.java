aea2b54c2846fe88faf93ca9d97898b850806e90
Josh Overbeck <overbejt@miamioh.edu>
2018-12-24 23:40:31 -0500

    created and used a jsp file

--- hello-World/src/main/java/webapp/LoginServlet.java
+++ hello-World/src/main/java/webapp/LoginServlet.java
@@ -3,6 +3,7 @@ package webapp;
 import java.io.IOException;
 import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
@@ -33,26 +34,9 @@ import javax.servlet.http.HttpServletResponse;
 public class LoginServlet extends HttpServlet {
 
 	@Override
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException {
-		PrintWriter out = response.getWriter();
-		out.println("<html>");
-		out.println("<head>");
-		out.println("<title>Yahoo!!!!!!!!</title>");
-		out.println("</head>");
-		out.println("<body>");
-		//me
-		out.println("<b>");
-		
-		out.println("My First Servlet");
-		
-		out.println("</b>");
-		out.println("<p>");
-		out.println("So this is how they do it.  I always wondered how this worked.  I think I understand now.");
-		out.println("</p>");
-		
-		//end me
-		out.println("</body>");
-		out.println("</html>");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
+		//I need to remember this line.  It is critical
+		request.getRequestDispatcher("/WEB-INF/views/login.jsp").forward(request, response);
 
 	}
 
