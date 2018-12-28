4470067da18a3e97c37eb26a1c2335442d0b7ec8
Josh Overbeck <overbejt@miamioh.edu>
2018-12-28 16:57:58 -0500

    Created a controller for the login page

---
+++ hello-World/src/main/java/com/udemyLesson/springmvc/LoginController.java
@@ -0,0 +1,10 @@
+package com.udemyLesson.springmvc;
+
+/**
+ * This is the class that will be the controller for the login page.  
+ * @author joshoverbeck
+ *
+ */
+public class LoginController {
+
+}//End of the 'LoginController' class
